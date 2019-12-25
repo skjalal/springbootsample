b16dc5faef4c96fe867af3589fe26d781612ef50
Shaikh Jalaluddin <sk.jalal46@gmail.com>
2019-12-25 12:35:36 +0530

    First Commit

---
+++ src/main/java/org/example/SpringBootSample.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSample {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSample.class, args);
+    }
+}
